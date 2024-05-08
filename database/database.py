50c4f97ef17804e57f844ff430305df08825eb80
carlos.m <carlosmarquezmolina1@gmail.com>
2024-05-08 20:16:52 +0200

    register function funciona, login de momento no

--- database/database.py
+++ database/database.py
@@ -2,26 +2,76 @@ import sqlite3
 
 def connect():
 
-    conn = sqlite3.connect('database.db')
+    # Conexión a la base de datos (creará un archivo de base de datos llamado 'biblioteca.db' si no existe)
+    conn = sqlite3.connect('biblioteca.db')
     cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            password TEXT NOT NULL
-        )
-    ''')
+
+    try:
+    # Creación de la tabla 'usuarios'
+        cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+
+                            nombre TEXT PRIMARY KEY,
+                            email TEXT,
+                            password TEXT,
+                            fecha_registro TEXT DEFAULT (DATETIME('now')),
+                            administrador INTEGER NULL DEFAULT 0
+                        )''')
+
+        # Creación de la tabla 'libros'
+        cursor.execute('''CREATE TABLE IF NOT EXISTS libros (
+                            id INTEGER PRIMARY KEY,
+                            titulo TEXT,
+                            autor TEXT,
+                            fecha_publicacion TEXT,
+                            genero TEXT,
+                            disponible INTEGER
+                        )''')
+
+        # Creación de la tabla 'libros_prestados'
+        cursor.execute('''CREATE TABLE IF NOT EXISTS libros_prestados (
+                            id INTEGER PRIMARY KEY,
+                            id_libro INTEGER,
+                            id_usuario INTEGER,
+                            fecha_prestamo TEXT,
+                            fecha_devolucion TEXT,
+                            devuelto INTEGER
+                        )''')
+    except sqlite3.Error as e:
+        print("Error al crear las tablas: ", e)
+
+    # Guardar cambios y cerrar la conexión
     conn.commit()
     conn.close()
     print("Conexión exitosa")
     return conn, cursor
 
-def insert_user(name, email, password):
-    conn, cursor = connect()
-    cursor.execute(f'''
-        INSERT INTO users(name, email, password)
-        VALUES('{name}', '{email}', '{password}')
-    ''')
-    conn.commit()
+
+def close(conn):
     conn.close()
+    print("Conexión cerrada")
+
+
+def insert_user(conn, cursor, nombre, email, password):
+    
+    try:
+        cursor.execute('''INSERT INTO usuarios (nombre, email, password)
+                        VALUES (?, ?, ?)''', (nombre, email, password))
+        conn.commit()
+        print("Usuario registrado")
+    except sqlite3.Error as e:
+        print("Error al registrar usuario: ", e)
+
+def login_user(conn, cursor, nombre, password):
+    try:
+        cursor.execute('''SELECT * FROM usuarios WHERE nombre = ? AND password = ?''', (nombre, password))
+        user = cursor.fetchone()
+        if user:
+            print("Inicio de sesión exitoso")
+            return True
+        else:
+            print("Credenciales incorrectas")
+            return False
+    except sqlite3.Error as e:
+        print("Error al iniciar sesión: ", e)
+        return False
+
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import sqlite3
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from plantillas.pantallaPrincipal import Ui_MainWindow 
+from database.database import close, connect, insert_user, login_user
+from plantillas.pantallaPrincipal import Ui_MainWindow as vPrincipal
+from plantillas.menuPrincipal import Ui_MainWindow as vMenuPrincipal
 
 from PySide6.QtWidgets import QApplication, QPushButton, QLineEdit, QLabel, QMainWindow, QWidget
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, vPrincipal):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = vPrincipal()
         self.ui.setupUi(self)
         self.ui.registerButtonPage.clicked.connect(self.show_register_page)
         self.ui.loginButtonPage.clicked.connect(self.show_login_page)
         self.ui.loginButton.clicked.connect(self.login)
         self.ui.registerButton.clicked.connect(self.register)
+        connect()
 
     def show_register_page(self):
         self.ui.stackedWidget.setCurrentIndex(1)
@@ -22,16 +26,53 @@ class MainWindow(QMainWindow):
         self.ui.stackedWidget.setCurrentIndex(0)
 
     def login(self):
-        # Aquí puedes implementar la lógica para el inicio de sesión
-        # Por ejemplo, obtener los datos de los QLineEdit y procesarlos
-        # Una vez iniciada la sesión, puedes alternar a la página de inicio
-        print("Iniciado sesión")
+        nombre = self.ui.usernameInputLogin.text()
+        password = self.ui.passwordInputLogin.text()
+
+        # Obtén la conexión y el cursor desde la función connect()
+        conn = sqlite3.connect('biblioteca.db')
+        cursor = conn.cursor()
+
+        # si el usuario existe, muestra la pantalla principal
+        if login_user(conn, cursor, nombre, password):
+            # Oculta la ventana de login
+            self.hide()
+            # Crear e iniciar la ventana del menú principal
+            self.menu_principal = menuPrincipal()
+            self.menu_principal.show()
+        
+
+
+
 
     def register(self):
-        # Aquí puedes implementar la lógica para el registro
-        # Por ejemplo, obtener los datos de los QLineEdit y procesarlos
-        # Una vez registrado, puedes alternar a la página de inicio de sesión
-        print("Registrado")
+
+        nombre = self.ui.usernameInputRegister.text()
+        email = self.ui.emailInput.text()
+        password = self.ui.passwordInputRegister.text()
+
+        # Obtén la conexión y el cursor desde la función connect()
+        conn = sqlite3.connect('biblioteca.db')
+        cursor = conn.cursor()
+
+        # Llama a la función insert_user() con los parámetros correctos
+        insert_user(conn, cursor, nombre, email, password)
+
+
+class menuPrincipal(QMainWindow, vMenuPrincipal):
+    def __init__(self):
+        super().__init__()
+        self.ui = vMenuPrincipal()
+        self.ui.setupUi(self)
+        self.ui.logoutButton.clicked.connect(self.logout)
+
+
+
+ 
+    
+
+
+
     
 
 if __name__ == "__main__":
